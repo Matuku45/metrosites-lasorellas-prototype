d3f4cc44e2e62919049db1286dffbe3555cf425e
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-10-21 20:59:44 +0200

    linked the services with the Router

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from "./pages/About.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Booking from "./pages/Booking.jsx";
-
+import Services from "./pages/Services.jsx";
 export default function App() {
   return (
     <Router>
@@ -15,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
